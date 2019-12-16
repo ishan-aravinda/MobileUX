ed54d221abe90a8982e311893980612551a03661
unknown <ishan.2016055@iit.ac.lk>
2019-12-16 16:00:21 +0530

    final

---
+++ js/firebase_init.js
@@ -0,0 +1,14 @@
+   // Your web app's Firebase configuration
+   var firebaseConfig = {
+       apiKey: "AIzaSyCiqITMCVsyHKwdbDgFCowNRwLsfLqiYOY",
+       authDomain: "train-cde96.firebaseapp.com",
+       databaseURL: "https://train-cde96.firebaseio.com",
+       projectId: "train-cde96",
+       storageBucket: "train-cde96.appspot.com",
+       messagingSenderId: "45911183152",
+       appId: "1:45911183152:web:b27375f04a3b109b5442f5",
+       measurementId: "G-B29GE7SWEX"
+   };
+   // Initialize Firebase
+   firebase.initializeApp(firebaseConfig);
+   firebase.database();
